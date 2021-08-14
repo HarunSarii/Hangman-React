767b2c07582e80de341a9445ee72be59419c2183
HarunSarii <harunsari14@gmail.com>
2021-08-14 12:47:46 +0300

    update files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react'
 import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure'
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Figure />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+        <Popup />
+        <Notification />
+      </div>
     </div>
   );
 }
---
+++ src/components/Word.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Word = () => {
+    return (
+        <div class="word" id="word"></div>
+    )
+}
+
+export default Word
