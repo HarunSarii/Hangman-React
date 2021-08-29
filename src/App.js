02fcacdbcabe1a3c8dc6fc2d22cfb8d87480f56c
HarunSarii <harunsari14@gmail.com>
2021-08-29 12:31:08 +0300

    add word

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Header from './components/Header';
 import Figure from './components/Figure'
@@ -7,15 +7,26 @@ import Word from './components/Word';
 import Popup from './components/Popup';
 import Notification from './components/Notification';
 
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+let playable = true;
+
+const correctLetters = [];
+const wrongLetters = [];
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
   return (
     <div className="App">
       <Header />
       <div className="game-container">
         <Figure />
         <WrongLetters />
-        <Word />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
         <Popup />
         <Notification />
       </div>
--- src/components/Word.js
+++ src/components/Word.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Word = () => {
+const Word = ({ selectedWord, correctLetters }) => {
     return (
-        <div class="word" id="word"></div>
+        <div class="word" id="word">
+            {
+                selectedWord.split('').map((letter, i) => {
+                    return (
+                        <span className='letter' key={i} >
+                            {correctLetters.includes(letter) ? letter : ''}
+                        </span>
+                    )
+                })
+            }
+        </div>
     )
 }
 
