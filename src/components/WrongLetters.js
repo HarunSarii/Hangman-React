3eb9d9147fbb1b1ae672def7293112b57b761063
HarunSarii <harunsari14@gmail.com>
2021-09-02 20:10:38 +0300

    add wrongLetters

--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -3,7 +3,13 @@ import React from 'react'
 const WrongLetters = () => {
     return (
         <div class="wrong-letters-container">
-            <div id="wrong-letters"></div>
+            <div id="wrong-letters">
+                {wrongLetters.length > 0 && <p>Wrong</p>}
+                {wrongLetters
+                    .map((letter, i) => <span key={i}>{letter}</span>)
+                    .reduce((prev, curr) => prev == null ? [curr] : [prev, ', ', curr], null)}
+
+            </div>
         </div>
     )
 }
