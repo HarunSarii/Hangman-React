912590299d168af7344f18c4a86bf5472b74fd8b
HarunSarii <harunsari14@gmail.com>
2021-09-05 18:15:03 +0300

    update Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { checkWin } from '../helpers/helper'
 
 const Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable }) => {
+    let finalMessage = '';
+    let finalMessageRevealWord = '';
+    let playable = true;
+
+    if (checkWin(correctLetters, wrongLetters, selectedWord) === 'win') {
+        finalMessage = 'Congratulations! You Won!';
+        playable = false;
+    } else if (checkWin(correctLetters, wrongLetters, selectedWord) === 'lose') {
+        finalMessage = 'Unfortunately you lost!';
+        finalMessageRevealWord = `... the word was : ${selectedWord}`;
+        playable = false;
+    }
+
+    useEffect(() => setPlayable(playable))
+
     return (
-        <div className="popup-container" >
+        <div className="popup-container" style={finalMessage !== '' ? { display: 'flex' } : {}} >
             <div className="popup">
                 <h2>{finalMessage}</h2>
                 <h3>{finalMessageRevealWord}</h3>
