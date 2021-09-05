49a92f9fa233fdd30f8c316659912b82d37e979b
HarunSarii <harunsari14@gmail.com>
2021-09-05 18:15:19 +0300

    update helper

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -5,3 +5,18 @@ export function showNotification(setter) {
     }, 2000);
 }
 
+export function checkWin(correct, wrong, word) {
+    let status = 'win';
+
+    //check for win
+    word.split('').forEach(letter => {
+        if (!correct.includes(letter)) {
+            status = ''
+        }
+    })
+    //check for lose
+
+    if (wrong.length === 6) status = 'lose'
+
+    return status
+}
